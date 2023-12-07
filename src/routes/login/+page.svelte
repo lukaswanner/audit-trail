568395a73b77dd8a6be37552f3aa7d921c447c2a
SponsoredByPuma <tillwanner01@gmail.com>
2023-12-07 10:39:48 +0100

    feature: creature with eyes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,51 @@
 
 	let successfulLogIn = true;
 	let closeToast = false;
+
+	function getMousePosition() {
+		window.addEventListener('mousemove', (event) => {
+			const eyesLeft = document.querySelector('.eyesLeft');
+			if (eyesLeft) {
+				const mouseX = event.clientX;
+				const mouseY = event.clientY;
+				const y = 5 + (mouseY * 2) / 100;
+				const x = 75 - (mouseX * 4) / 100;
+				eyesLeft.style.top = `${y}%`;
+				eyesLeft.style.right = `${x}%`;
+			}
+			const eyesRight = document.querySelector('.eyesRight');
+			if (eyesRight) {
+				const mouseX = event.clientX;
+				const mouseY = event.clientY;
+				const y = 5 + (mouseY * 2) / 100;
+				const x = 55 - (mouseX * 4) / 100;
+				eyesRight.style.top = `${y}%`;
+				eyesRight.style.right = `${x}%`;
+			}
+
+			const eyeR = document.querySelector('.eyeR');
+			if (eyeR) {
+				const mouseX = event.clientX;
+				const mouseY = event.clientY;
+				const y = 5 + (mouseY * 2) / 100;
+				const x = 55 - (mouseX * 4) / 100;
+				eyeR.style.top = `${y}%`;
+				eyeR.style.right = `${x}%`;
+			}
+
+			const eyeL = document.querySelector('.eyeL');
+			if (eyeL) {
+				const mouseX = event.clientX;
+				const mouseY = event.clientY;
+				const y = 5 + (mouseY * 2) / 100;
+				const x = 55 - (mouseX * 4) / 100;
+				eyeL.style.top = `${y}%`;
+				eyeL.style.right = `${x}%`;
+			}
+		});
+	}
+
+	getMousePosition();
 </script>
 
 {#if !successfulLogIn && !closeToast}
@@ -37,10 +82,14 @@
 	<LoginCard bind:successfulLogIn bind:closeToast />
 	<div class="hero min-h-screen bg-base-200">
 		<div class="hero-content flex-col lg:flex-row">
-			<img
-				src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-				class="max-w-sm rounded-lg shadow-2xl"
-			/>
+			<div class="test">
+				<div class="eyesLeft">
+					<div class="eyeL"></div>
+				</div>
+				<div class="eyesRight">
+					<div class="eyeR"></div>
+				</div>
+			</div>
 			<div>
 				<h1 class="text-5xl font-bold">Audit Trail!</h1>
 				<p class="py-6">
@@ -52,3 +101,53 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	.test {
+		width: 10vw;
+		height: 500px;
+		border: solid;
+		border-color: cyan;
+		position: relative;
+	}
+
+	.eyesLeft {
+		border-radius: 50%;
+		height: 30px;
+		width: 30px;
+		border: solid;
+		border-color: white;
+		background-color: white;
+		position: absolute;
+	}
+
+	.eyesRight {
+		border-radius: 50%;
+		height: 30px;
+		width: 30px;
+		border: solid;
+		border-color: white;
+		background-color: white;
+		position: absolute;
+	}
+
+	.eyeR {
+		border-radius: 50%;
+		height: 15px;
+		width: 15px;
+		border: solid;
+		border-color: black;
+		background-color: black;
+		position: absolute;
+	}
+
+	.eyeL {
+		border-radius: 50%;
+		height: 15px;
+		width: 15px;
+		border: solid;
+		border-color: black;
+		background-color: black;
+		position: absolute;
+	}
+</style>
