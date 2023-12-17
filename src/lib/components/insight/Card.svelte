6c4897e0bfabdc5a7765fd5d95861749f09e2d60
Lukas Wanner <lukas_wanner@outlook.de>
2023-12-17 18:08:28 +0100

    feat: added cards

---
+++ src/lib/components/insight/Card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let title: string;
+	export let count: string;
+	export let emoji: string;
+</script>
+
+<div class="flex flex-col items-start rounded-2xl bg-neutral-content/20 border border-neutral-content/50 p-4">
+	<p class="h-8 w-8 self-end text-3xl">{emoji}</p>
+	<p class="text-xl font-bold brightness-150">{count}</p>
+	<p class="text-md">{title}</p>
+</div>
--- src/routes/(private)/insights/+page.svelte
+++ src/routes/(private)/insights/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+	import Card from '$lib/components/insight/Card.svelte';
+</script>
+
 <div class="flex flex-row justify-between border-b border-b-base-content/10 p-4">
-	<h1 class="text-3xl font-bold">insights</h1>
+	<h1 class="text-3xl font-bold brightness-150">insights</h1>
+</div>
+
+<div class="grid grid-cols-[1fr_1fr] gap-4 p-4">
+	<Card title="Online" count="27" emoji="🟢" />
+	<Card title="24h sales" count="$2,250" emoji="💲" />
+	<Card title="orders in transit" count="12" emoji="🚚" />
+	<Card title="orders processing" count="23" emoji="🏭" />
 </div>
